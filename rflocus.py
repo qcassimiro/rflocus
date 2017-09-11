07a3a4a884f460f6dde1f5f0dd58909cbb78fc59
Andre Gabriel de Andrade Cassimiro <andre.cassimiro@TRC.local>
2017-09-11 10:14:31 -0300

    added logging and logging configuration

--- rflocus.py
+++ rflocus.py
@@ -6,6 +6,9 @@ import logging
 import os
 import sqlite3
 import sys
+import yaml
+
+import logging.config
 
 import flask
 import flask_cors
@@ -13,6 +16,7 @@ import flask_restful
 
 
 class RFLocus(flask_restful.Resource):
+
     def __init__(self):
         """ init shit """
         pass
@@ -25,7 +29,9 @@ class RFLocus(flask_restful.Resource):
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Descricao curta do programa")
+    parser = argparse.ArgumentParser(description='''
+        Descricao curta do programa
+        ''')
     parser.add_argument("-v",
                         "--verbose",
                         action='store_true',
@@ -35,22 +41,33 @@ def get_args():
                         action='store',
                         type=int,
                         default=5000,
-                        metavar="[1000-8000]",
+                        metavar="[5000-5500]",
                         help="Ajuda da opcao")
     args = vars(parser.parse_args())
     # verificar e/ou adicionar argumentos
     print(args)
-    if args['port'] < 1000 or args['port'] > 8000:
+    if args['port'] < 5000 or args['port'] > 5500:
         # logar erro de valor
         args = None
-    args['machine'] = os.uname().machine
+    args['plaform'] = sys.platform
     return args
+
+
+def set_logging(config_path='logging.yaml', level=logging.INFO):
+    if os.path.exists(config_path):
+        with open(config_path, 'rt') as f:
+            config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=level)
 
 
 def main():
     args = get_args()
     if not args:
         return 1
+    set_logging()
+    logging.info('Main porra')
     app = flask.Flask(__name__)
     flask_cors.CORS(app)
     api = flask_restful.Api(app)
